d930a404e8a222ae6e9b8646ddcb79be7f7a6793
Artur Sakhbiev <56787456+Takoivot@users.noreply.github.com>
2022-04-28 17:50:31 +0300

    Update Model.swift

--- ProfileLogInApp/Model.swift
+++ ProfileLogInApp/Model.swift
@@ -5,4 +5,27 @@
 //  Created by Артур Сахбиев on 28.04.2022.
 //
 
-import Foundation
+struct InfoUser {
+    let userName: String
+    let userPassword: String
+    let human: Human
+    
+    static func getInfoUser() -> InfoUser {
+        InfoUser(userName: "Artur", userPassword: "qwerty", human: Human.getHuman())
+    }
+}
+
+struct Human {
+    let name: String
+    let surname: String
+    let photo: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+    
+    static func getHuman() -> Human {
+        Human(name: "Artur", surname: "Sakhbiev", photo: "ArturS")
+    }
+}
+
