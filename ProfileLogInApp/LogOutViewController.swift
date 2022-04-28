69a9928b79b0e74cfd472f3d4a5217bc4a7ad321
Artur Sakhbiev <56787456+Takoivot@users.noreply.github.com>
2022-04-28 17:50:28 +0300

    Update LogOutViewController.swift

--- ProfileLogInApp/LogOutViewController.swift
+++ ProfileLogInApp/LogOutViewController.swift
@@ -9,21 +9,15 @@ import UIKit
 
 class LogOutViewController: UIViewController {
 
+    
+    @IBOutlet var welcomeUser: UILabel!
+    
+    var welcomeU: String = ""
+    var user: InfoUser!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        welcomeUser.text = "Welcome, \(user.human.fullName)"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
